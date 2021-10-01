ae99c102d7b23eca7e3686962352c284462196f4
xiaodeng <927561237@qq.com>
2021-10-01 23:16:48 +0800

    mapDispatchToProps简写形式（对象）

---
+++ 06_src_mapDispatch简写形式（对象）/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Count from './containers/Count'
+import store from './redux/store'
+
+export default function App() {
+  return (
+    <div>
+      <Count store={store} />
+    </div>
+  )
+}
