c0594db2de146701d447e8465afe2d62a46ab451
xiaodeng <927561237@qq.com>
2021-10-01 18:24:13 +0800

    异步action

---
+++ 04_src_异步action/redux/count_reducer.js
@@ -0,0 +1,20 @@
+/* 
+  1 改文件是用于创建一个为 Count 组件服务的 reducer，reducer的本质是一个函数
+  2 reducer 函数会接收两个参数，分别为：之前的状态（preState），动作对象（action）
+*/
+import { INCREMENT, DECREMENT } from './constant'
+
+// 初始化状态
+const initValue = 0
+export default function reducer(preValue = initValue, action) {
+  console.log(preValue)
+  const { data, type } = action
+  switch (type) {
+    case INCREMENT:
+      return preValue + data * 1
+    case DECREMENT:
+      return preValue - data * 1
+    default:
+      return preValue
+  }
+}
